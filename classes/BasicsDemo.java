0aa1b7c2bb5941e2a96cec52ce58ac1a347689c6
oluwatobiloba <oluwateezzy03@gmail.com>
2026-05-20 09:34:43 +0100

    feat: demonstrate floating-point precision issues and implement BigDecimal fix in BasicsDemo

--- classes/BasicsDemo.java
+++ classes/BasicsDemo.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 public class BasicsDemo {
     static void print() {
         System.out.println("\n\nInside print...");
@@ -22,5 +24,22 @@ public class BasicsDemo {
 
     public static void main(String[] args) {
         primitives();
+
+        System.out.println(1 - 0.9);
+        System.out.println(0.1 + 0.2);
+
+        double price = 1000;
+        double discountPercent = 0.9;
+        double discountAmount = price * discountPercent;
+
+        System.out.println(discountAmount);
+        System.out.println(price - discountAmount);
+        System.out.println(price * (1 - discountPercent));
+
+
+        BigDecimal first = new BigDecimal("0.1");
+        BigDecimal second = new BigDecimal("0.2");
+        System.out.println(first.add(second));
+
     }
 }
