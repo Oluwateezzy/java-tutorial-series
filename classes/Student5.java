f6efd37ac2c016afc20dfa1a12f7ecfe5932d961
oluwatobiloba <oluwateezzy03@gmail.com>
2026-05-23 14:31:44 +0100

    feat: add Student5 class to calculate tuition fees and track computation count

---
+++ classes/Student5.java
@@ -0,0 +1,43 @@
+public class Student5 {
+
+    static int computeCount;
+    String name;
+    String gender;
+    int age;
+    boolean international;
+
+    void compute() {
+        computeCount = computeCount + 1;
+
+        double internationalfee = 500.0;
+        double tutionFees = 1200.0;
+
+        System.out.println("name " + name);
+        System.out.println("gender " + gender);
+        System.out.println("age " + age);
+        System.out.println("computeCount " + computeCount);
+
+        if (international) {
+            tutionFees = tutionFees + internationalfee;
+        }
+
+        System.out.println("tutionFees " + tutionFees + "\n");
+    }
+
+    public static void main(String[] args) {
+        Student5 student = new Student5();
+        student.name = "john";
+        student.gender = "male";
+        student.age = 18;
+        student.international = true;
+        student.compute();
+
+        Student5 student1 = new Student5();
+        student1.name = "ola";
+        student1.gender = "male";
+        student1.age = 18;
+        student1.compute();
+
+         System.out.println("Student compute " + Student5.computeCount);
+    }
+}
