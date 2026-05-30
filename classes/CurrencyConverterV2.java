c4d3c3e6e84a4b37f12df27813a3821edd49ffdd
oluwatobiloba <oluwateezzy03@gmail.com>
2026-05-30 11:34:46 +0100

    refactor: encapsulate exchange rates and add dynamic update and computation methods to CurrencyConverterV2

--- classes/CurrencyConverterV2.java
+++ classes/CurrencyConverterV2.java
@@ -1,13 +1,21 @@
 public class CurrencyConverterV2 {
-    double[] exchangeRates = new double[] {
-        63.0,
-        3.0,
-        3.0,
-        595.0,
-        18.0,
-        107.0,
-        2.0
-    };
+    double[] exchangeRates;
+
+    void setExchangeRates(double[] rates) {
+        exchangeRates = rates;
+    }
+
+    void updateExchangeRate(int arrayIndex, double newVal) {
+        exchangeRates[arrayIndex] = newVal;
+    }
+
+    double getExchangeRate(int arrayIndex) {
+        return exchangeRates[arrayIndex];
+    }
+
+    double computeTransferAmount(int arrayIndex, double amount) {
+        return amount * getExchangeRate(arrayIndex);
+    }
 
     void printCurrencies() {
         System.out.println("rupee: " + exchangeRates[0]);
@@ -16,11 +24,38 @@ public class CurrencyConverterV2 {
         System.out.println("chilean_peso: " + exchangeRates[3]);
         System.out.println("mexican_peso: " + exchangeRates[4]);
         System.out.println("_yen: " + exchangeRates[5]);
-        System.out.println("$australian: " + exchangeRates[exchangeRates.length - 1]);
+        System.out.println("$australian: " + exchangeRates[exchangeRates.length - 1] + "\n");
     }
 
     public static void main(String[] args) {
-        CurrencyConverterV2 currencyConverter =  new CurrencyConverterV2();
+        CurrencyConverterV2 currencyConverter = new CurrencyConverterV2();
+        double[] rates = new double[] {
+                63.0,
+                3.0,
+                3.0,
+                595.0,
+                18.0,
+                107.0,
+                2.0
+        };
+        currencyConverter.setExchangeRates(rates);
         currencyConverter.printCurrencies();
+
+        rates = new double[] {
+                65.0,
+                3.0,
+                3.0,
+                595.0,
+                18.0,
+                107.0,
+                2.0
+        };
+        currencyConverter.setExchangeRates(rates);
+        currencyConverter.printCurrencies();
+
+        currencyConverter.updateExchangeRate(0, 32.0);
+        currencyConverter.printCurrencies();
+
+        System.out.println("\n Transfer Amount " + currencyConverter.computeTransferAmount(0, 337));
     }
 }
